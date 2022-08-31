63cd5de86fe3821468a47396df36caebeaeb3008
avramanaareddy <avramanaareddy@yahoo.com>
2022-08-30 21:46:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OrderBooks',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
